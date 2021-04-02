f756cc82f4c8ef46d4927ff74a074c4037ad2655
Calvin Li <lixx4090@mylaurier.ca>
2021-04-01 21:20:29 -0400

    Initial Commit

---
+++ StockDividendTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockDividendTracker
+//
+//  Created by Calvin Li on 2021-04-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
